4544cfb7c4723e85c47f455b89b3e0a2a2302c8a
Hasan Yücel <yclhasan00@gmail.com>
2020-05-10 01:02:30 +0200

    Latest version for gcd

--- source/tests.cpp
+++ source/tests.cpp
@@ -6,12 +6,21 @@ int gcd(int a, int b)
 {
     if (a == 0 && b == 0)
         return -1;
+    if (a == 0 || b == 0)
+        return 0;
+    else if (a == b)
+        return a;
+    else if (a > b)
+        return gcd(a - b, b);
+
+    else return gcd(a, b - a);
 }
 
 
 TEST_CASE("describe_gcd", "[gcd]")
 {
     REQUIRE(gcd(0, 0) == -1);
+    REQUIRE(gcd(0, 5) == 0);
     REQUIRE(gcd(2, 4) == 2);
     REQUIRE(gcd(9, 6) == 3);
     REQUIRE(gcd(3, 7) == 1); 
