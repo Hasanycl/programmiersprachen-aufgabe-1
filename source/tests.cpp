8f9da4cd278f8b5ff24a26c3373037939819cb03
Hasan Yücel <yclhasan00@gmail.com>
2020-05-10 00:28:54 +0200

    0 exception for gcd

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,7 +1,24 @@
 #define CATCH_CONFIG_RUNNER
-#include <catch.hpp>
+#include "catch.hpp"
+#include <cmath>
+
+int gcd(int a, int b)
+{
+    if (a == 0 && b == 0)
+        return -1;
+}
+
+
+TEST_CASE("describe_gcd", "[gcd]")
+{
+    REQUIRE(gcd(0, 0) == -1);
+    REQUIRE(gcd(2, 4) == 2);
+    REQUIRE(gcd(9, 6) == 3);
+    REQUIRE(gcd(3, 7) == 1); 
+}
+
 
 int main(int argc, char* argv[])
-{
-  return Catch::Session().run(argc, argv);
+{ 
+    return Catch::Session().run(argc, argv); 
 }
