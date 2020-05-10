e7890e4e8f08dd13570cfa548e0249bdae56d0b1
Hasan Yücel <yclhasan00@gmail.com>
2020-05-10 16:07:18 +0200

    Latest version of fract

--- source/fract_2.cpp
+++ source/fract_2.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
 
-float fract(float in_float_number) {
-	float result = in_float_number - int(in_float_number);
+float fract(float number) {
+	float result = number - int(number);
 	return result;
 }
 
-int main() {
 
-	float zahl = 5.123f;
+	TEST_CASE("describe_fract", "[fract]")
+	{
+		REQUIRE(fract(5.123f) == Approx(0.123f));
+		REQUIRE(fract(7.765f) == Approx(0.765f));
+	}
 
-	std::cout << "Der Nachkommaanteil von " << zahl << " ist: "
-		<< fract(zahl) << std::endl;
-
-	return 0;
-}
+	int main(int argc, char* argv[])
+	{
+		return Catch::Session().run(argc, argv);
+	}
